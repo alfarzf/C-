30a174b73cc12b3ecffbaa010a9962d68798cfdc
Alfa Rizki Fadlillah <92286829+alfarzf@users.noreply.github.com>
2021-11-11 08:10:03 +0700

    Create Prime Numbers.cpp

---
+++ Prime Numbers.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+
+	int x;
+  int i;
+  bool prime=true;
+  
+  //Input
+  cout<<"Masukkan Bilangan Bulat Positif"<<endl;
+  cin>>x;
+ 
+  //Output
+  if(x==0||x==1){
+    prime=false;
+  }
+  else{
+    for(i=2;i<=x/2;i++){
+      if(x%i==0){
+        prime=false;
+        break;
+      }
+    }
+  }
+ 
+  
+  if (prime){
+  	cout<<x<<" adalah angka prima";
+	}
+  else{
+    cout<<x<<" bukan angka prima";
+	}
+ return 0;
+}
