df57ffcd104c2c6b86b6b235f17bcea67f1c132d
Alfa Rizki Fadlillah <92286829+alfarzf@users.noreply.github.com>
2021-11-11 08:08:21 +0700

    Create Sorting Ex.cpp

---
+++ Sorting Ex.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ncurses.h>
+using namespace std;
+
+int main(){
+	int n;
+	int temp=0;
+	cin>>n;
+	int angka[n];
+	for(int i=0;i<n;i++){
+		cin>>angka[i];
+	}for(int i=0;i<n;i++){
+		for(int k=0;k<n-1;k++){
+			if(angka[k]>angka[k+1]){
+				temp=angka[k];
+				angka[k]=angka[k+1];
+				angka[k+1]=temp;
+			}
+		}
+	}for(int i=0;i<n;i++){
+		cout<<angka[i];
+		cout<<" ";
+	}
+	return 0;
+}
